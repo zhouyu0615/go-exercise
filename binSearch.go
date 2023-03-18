ff690c80a6cd7861b89e3166f3435981f4cf84cd
zhouyu0615 <871211719@qq.com>
2023-03-18 22:25:50 +0800

    test

--- binSearch.go
+++ binSearch.go
@@ -1,31 +1,28 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
-
-func binSearch( arr []int, target int) int {
-	low  := 0
-	high := len(arr) - 1;
+func binSearch(arr []int, target int) int {
+	low := 0
+	high := len(arr) - 1
 	mid := 0
-	for low < high {
-		mid = low + (high - mid) / 2;
-		if (arr[mid] == target){
+	for low <= high {
+		mid = low + (high-mid)/2
+		if arr[mid] == target {
 			return mid
-		} else if (arr[mid] < target) {
+		} else if arr[mid] < target {
 			low = mid + 1
-		} else if (arr[mid] > target){
+		} else if arr[mid] > target {
 			high = mid - 1
 		}
 	}
-	return -1;
+	return -1
 }
 
 func main() {
-	items := []int{1,2, 9, 20, 31, 45, 63, 70, 100}
+	items := []int{1, 2, 9, 20, 31, 45, 63, 70, 100}
 
-	result := binSearch(items, 31);
+	result := binSearch(items, 31)
 	fmt.Println("get result ", result)
 
 }
